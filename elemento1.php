aa44bc518e0d07333e0dc01db496d0a81c792fd7
Ernesto Campos Villa <misterernest@hotmail.com>
2017-11-16 17:03:49 -0500

    Add structure html

--- elemento1.php
+++ elemento1.php
@@ -1,3 +1,14 @@
-primer cambio re3alizado
-segundo cambio realizado
-tercer cambio sin tener coexion a github
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <h1>primer cambio re3alizado</h1>
+  <h2>segundo cambio realizado</h2>
+  <h3>tercer cambio sin tener coexion a github</h3>
+</body>
+</html>
